e6323c98dfc9a228687199909a65c1cac3b24bb1
Pedro Brick <pedrobrick@hotmail.com>
2020-09-09 16:23:35 -0300

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const transactionRouter = express.Router();
+const services = require("../services/transactionService.js");
+
+transactionRouter.get("/findAll/", services.findAll);
+transactionRouter.post("/create/", services.create);
+transactionRouter.put("/update/", services.update);
+transactionRouter.delete("/remove/", services.remove);
+transactionRouter.get("/filterByDescription/", services.filterByDescription);
+
+
+module.exports = transactionRouter;
